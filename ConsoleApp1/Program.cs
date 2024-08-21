ed8bf5986839da1670164b0b0492a0dc57596476
Ludwig Lennartsson <ludwig.lennartsson@skola.sala.se>
2024-08-21 15:06:20 +0200

    klar

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,25 +14,34 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             gissa();
-
-            Console.WriteLine("Vill du fortsätta (Y/N)");
-            string yn = Console.ReadLine();
-            if (yn == "y" || yn == "Y")
-            {
-                Console.Clear();
-                gissa();
-            }
-            else if (yn == "n" || yn == "N")
-            {
-                Environment.Exit(0);
-            }
-
         }
 
         public static bool gissa()
         {
+            int diff = 20;
+            Console.WriteLine("Välj svårighetsgrad: (E/M/H)");
+            string difficulty = Console.ReadLine();
+            
+            if (difficulty == "e" || difficulty == "E")
+            {
+                diff = 20;
+            }
+            else if (difficulty == "m" || difficulty == "M")
+            {
+                diff = 50;
+            }
+            else if (difficulty == "h" || difficulty == "H")
+            {
+                diff = 100;
+            }
+            else if (difficulty == "superdupersecret")
+            {
+                Console.WriteLine("Skriv in eget nummer över 0");
+                diff = int.Parse(Console.ReadLine());
+            }
+
             Random rnd = new Random();
-            int number = rnd.Next(0, 20);
+            int number = rnd.Next(0, diff);
             bool win = false;
 
             while (win == false)
@@ -52,6 +62,24 @@ namespace ConsoleApp1
                     Console.WriteLine("v");
                 }
             }
+
+            if (diff > 1000)
+            {
+                Console.WriteLine("hooray!!");
+            }
+
+            Console.WriteLine("Vill du fortsätta (Y/N)");
+            string yn = Console.ReadLine();
+            if (yn == "y" || yn == "Y")
+            {
+                Console.Clear();
+                gissa();
+            }
+            else if (yn == "n" || yn == "N")
+            {
+                Environment.Exit(0);
+            }
+
             return win = true;
         }
     }
